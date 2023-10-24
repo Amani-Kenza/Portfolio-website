3138dfe0e06364a3ebd92df736b35073d4a7b16d
Amani-Kenza <kenza.amani@fgei.ummto.dz>
2023-10-24 05:32:47 +0100

    'hi'

---
+++ api/server.js
@@ -0,0 +1,38 @@
+const nodemailer = require("nodemailer");
+
+export default async (req, res) => {
+  if (req.method === "POST") {
+    const { firstName, lastName, email, phone, message } = req.body;
+    const name = firstName + lastName;
+    
+    const mail = {
+      from: name,
+      to: "amanikenza7@gmail.com",
+      subject: "Contact Form Submission - Portfolio",
+      html: `<p>Name: ${name}</p>
+             <p>Email: ${email}</p>
+             <p>Phone: ${phone}</p>
+             <p>Message: ${message}</p>`,
+    };
+
+    const contactEmail = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "amanikenza7@gmail.com",
+        pass: "fbqompxhjbefkcfz",
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+
+    try {
+      await contactEmail.sendMail(mail);
+      res.status(200).json({ message: "Message sent successfully" });
+    } catch (error) {
+      res.status(500).json({ message: "Something went wrong, please try again later" });
+    }
+  } else {
+    res.status(405).end();
+  }
+};
