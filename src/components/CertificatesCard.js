2d98dfa88c5a925937e505ffdb98ac8e38ee9536
SapphireHacks <sapphirehacks@gmail.com>
2024-05-06 07:04:06 +0100

    modifed

--- src/components/CertificatesCard.js
+++ src/components/CertificatesCard.js
@@ -5,10 +5,6 @@ export const CertificatesCard = ({ title, description, imgUrl }) => {
     <Col xs={12} sm={6} md={4}>
       <div className="proj-imgbxx" >
         <img src={imgUrl} alt="img" className="custom-image"/>
-        <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
-        </div>
       </div>
     </Col>
   )
